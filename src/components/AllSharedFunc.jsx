e767c28c14cf0627feba379e83e75e2e294ffe5e
mdmahediTech <mehedihasan51020510@gmail.com>
2026-05-22 03:47:58 -0700

    profile page done

--- src/components/AllSharedFunc.jsx
+++ src/components/AllSharedFunc.jsx
@@ -7,22 +7,3 @@ export const ForHomePage=async ()=>{
   return res.json();
 }
 
-// sort start
-    // const [sortingtype,setsortingtype]=useState('');
-    // console.log(sortingtype);
-
-    //  {/* sort start */}
-    //      <div className="dropdown dropdown-bottom flex justify-center py-5">
-    //       <div tabIndex={0} role="button" className="btn m-1">Click ⬇️</div>
-    //       <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-    //         <li onClick={setsortingtype("price")}><a>price</a></li>
-           
-    //       </ul>
-    //     </div>
-                {/* shot field end */}
-
-// for details page
-// export const ForDetails=async ()=>{
-//   const res=await fetch(`http://localhost:5000/products/${id}`)
-//   return res.json();
-// }
